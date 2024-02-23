09b707a272873a9b5ed4129f6d4ce8166f662453
CEduardo <c-eduardo94@hotmail.com>
2024-02-23 17:22:27 -0300

    Conceito e exercicios

---
+++ teste_mesa_while/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x = 4;
+		int y = 0;
+		int i = 0;
+		
+		while(i < x) {
+			i = i + 1;
+			y = y + i;
+			System.out.println(i + "-" + y);
+			
+		}
+		
+
+	}
+
+}
